336e5eb9ffb8fe261b8c21c0c0b272ef7a95e0ce
PLUJA <64632615+pluja@users.noreply.github.com>
2020-08-23 11:10:10 +0200

    Get channel info and videos by id

--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -10,6 +10,60 @@ class YoutubeSearch:
 
         self.videos = self.search_videos()
         self.channels = self.search_channels()
+        
+
+    def channelInfo(id):
+        print("hi, searching for: {}".format(id))
+        encoded_search = urllib.parse.quote(id)
+        BASE_URL = "https://youtube.com"
+        url = f"{BASE_URL}/channel/{encoded_search}/videos"
+        response = requests.get(url).text
+
+        while 'window["ytInitialData"]' not in response:
+            response = requests.get(url).text
+
+        results = []
+        start = (
+            response.index('window["ytInitialData"]')
+            + len('window["ytInitialData"]')
+            + 3
+        )
+        end = response.index("};", start) + 1
+        json_str = response[start:end]
+        data = json.loads(json_str)
+        videoContent = data["contents"]["twoColumnBrowseResultsRenderer"]['tabs'][1]['tabRenderer']['content'][
+            'sectionListRenderer']['contents'][0]['itemSectionRenderer'][
+            'contents'][0]['gridRenderer']['items']
+        channelDetails = data["header"]['c4TabbedHeaderRenderer']
+
+        try:
+            sC = channelDetails['subscriberCountText']['simpleText'].split(" ")[0]
+        except:
+            sC = "unknown"
+        channel = {
+            'id': id,
+            'name': channelDetails['title'],
+            'avatar': channelDetails['avatar']['thumbnails'][2]['url'],
+            'subCount': sC
+        }
+        
+        videos = []
+        for video in videoContent:
+            vid = {
+                'videoTitle': video['gridVideoRenderer']['title']['runs'][0]['text'],
+                'id': video['gridVideoRenderer']['videoId'],
+                'channelName': channelDetails['title'],
+                'views': video['gridVideoRenderer']['publishedTimeText']['simpleText'],
+                'timeStamp': video['gridVideoRenderer']['viewCountText']['simpleText'],
+                'videoThumb': video['gridVideoRenderer']['thumbnail']['thumbnails'][0]['url'],
+                'channelUrl': "/channel/{}".format(id)
+            }
+            videos.append(vid)
+
+        results.append(channel)
+        results.append(videos)
+
+        return results
 
     def search_videos(self):
         encoded_search = urllib.parse.quote(self.search_terms)
@@ -34,6 +88,7 @@ class YoutubeSearch:
         if self.max_results is not None and len(results) > self.max_results:
             return results[: self.max_results]
         return results
+
 
     def parse_html_channels(self, response):
         results = []
@@ -85,21 +140,37 @@ class YoutubeSearch:
         videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
             "sectionListRenderer"
         ]["contents"][0]["itemSectionRenderer"]["contents"]
-
         for video in videos:
             res = {}
+
+            # IF IT IS A LIVESTREAM
+            if "playlistRenderer" in video.keys():
+                continue
+            
+            # IF IT IS A VIDEO:
             if "videoRenderer" in video.keys():
                 video_data = video.get("videoRenderer", {})
-                res["id"] = video_data.get("videoId", None)
-                res["thumbnails"] = [thumb.get("url", None) for thumb in video_data.get("thumbnail", {}).get("thumbnails", [{}]) ]
-                res["title"] = video_data.get("title", {}).get("runs", [[{}]])[0].get("text", None)
-                res["long_desc"] = video_data.get("descriptionSnippet", {}).get("runs", [{}])[0].get("text", None)
-                res["channel"] = video_data.get("longBylineText", {}).get("runs", [[{}]])[0].get("text", None)
-                res["duration"] = video_data.get("lengthText", {}).get("simpleText", 0)
-                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0).split(" ")[0]
-                res['publishedText'] = video_data.get("publishedTimeText", None).get("simpleText")
-                res["url_suffix"] = video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
-                results.append(res)
+
+                # CHECK IF IT IS A LIVESTREAM (Support for livestreams will be added in the future)
+                try:
+                    if "BADGE_STYLE_TYPE_LIVE_NOW" == video_data.get("badges")[0].get('metadataBadgeRenderer').get("style"):
+                        continue
+                
+                # IF IT IS NOT A LIVESTREAM, GET THE VIDEO
+                except:
+                    res["id"] = video_data.get("videoId", None)
+                    res["thumbnails"] = [thumb.get("url", None) for thumb in video_data.get("thumbnail", {}).get("thumbnails", [{}]) ]
+                    res["title"] = video_data.get("title", {}).get("runs", [[{}]])[0].get("text", None)
+                    res["long_desc"] = video_data.get("descriptionSnippet", {}).get("runs", [{}])[0].get("text", None)
+                    res["channel"] = video_data.get("longBylineText", {}).get("runs", [[{}]])[0].get("text", None)
+                    res["duration"] = video_data.get("lengthText", {}).get("simpleText", 0)
+                    res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0).split(" ")[0]
+                    try:
+                        res['publishedText'] = video_data.get("publishedTimeText", None).get("simpleText")
+                    except:
+                        res['publishedText'] = "unavailable"
+                    res["url_suffix"] = video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
+                    results.append(res)
         return results
 
     def videos_to_dict(self):
